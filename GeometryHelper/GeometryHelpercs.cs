6adb3388a79893e6972ff1df8bddf84c344b21e1
Murad491940 <84373217+Murad491940@users.noreply.github.com>
2023-06-10 00:52:09 +0400

    GeometryTask

---
+++ GeometryHelper/GeometryHelpercs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryHelper
+{
+    static class GeometryHelpercs
+    {
+        public static void SquareArae(int a)
+        {
+            Console.WriteLine ("Kvadratin sahesi: "+a * a);
+        }
+        public static void TriangeArae(int a, int b,int c)
+        {
+            Console.WriteLine("Ucbucaqin perimetri: "+(a + b + c)/2);
+        }
+        
+    }
+}
---
+++ GeometryHelper/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GeometryHelper
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GeometryHelpercs.SquareArae(5);
+            GeometryHelpercs.TriangeArae(5, 8, 10);
+            
+
+        }
+    }
+}
